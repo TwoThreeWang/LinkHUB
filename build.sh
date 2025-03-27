0b3708c11b33e864ba5a7bbc6a211b287e007317
WangTwoThree <twothreewang@gmail.com>
2025-03-27 16:49:08 +0800

    移除 `docker build` 命令中的 `--parallel` 选项

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 echo '开始拉取最新代码'
 git pull origin main
 echo '打包镜像'
-DOCKER_BUILDKIT=1 docker build --parallel -t link-hub:latest .
+DOCKER_BUILDKIT=1 docker build -t link-hub:latest .
 echo '启动容器'
 docker-compose down
 docker-compose up --force-recreate -d --remove-orphans
